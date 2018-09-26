c8a159aaf6f19e61ba90a2e195e98cd1b65d83c0
Wainercrb <2011-peswainer.1996@gmail.com>
2018-09-26 16:43:03 -0600

    update server routes

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/client-chamberos-cr'));
+app.use(express.static(__dirname + '/dist/clientChamberosCR'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/client-chamberos-cr/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/clientChamberosCR/index.html'));
 });
 // Start the app by listening on the default Heroku port
 var listener = app.listen(process.env.PORT || 8080, function(){
